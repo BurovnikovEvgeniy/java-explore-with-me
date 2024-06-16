b60625fe0b34f48d2d58018862010215931c08d8
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-16 23:58:27 +0300

    Fix import

--- stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -9,7 +9,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.ResourceBundle;
 
 @Service
 @RequiredArgsConstructor
