98155e8e42277dc75138ebf6f81653d5baabffa0
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-17 01:21:14 +0300

    Fix import

--- stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
