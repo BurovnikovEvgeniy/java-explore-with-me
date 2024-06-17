bb559c358fd8cdbe88662a222590927b71b99f20
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-17 18:13:46 +0300

    Delete extra variable

--- stats-service/client/src/main/java/ru/practicum/ewm/BaseClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/BaseClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -10,11 +9,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class BaseClient {
 
-    @Value("${stats-server.url}")
-    private String baseUrl;
-
     @Bean
     public WebClient webClient() {
+        //    @Value("${stats-server.url}")
+        String baseUrl = "http://localhost:9090";
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
