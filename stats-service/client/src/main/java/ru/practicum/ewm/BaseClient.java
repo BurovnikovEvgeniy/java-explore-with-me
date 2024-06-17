f3d89ffa23fd1a73fe6a44ff9634896cda007ad3
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-17 17:56:40 +0300

    Fix annotation

--- stats-service/client/src/main/java/ru/practicum/ewm/BaseClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/BaseClient.java
@@ -2,12 +2,12 @@ package ru.practicum.ewm;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
+@Configuration
 public class BaseClient {
 
     @Value("${stats-server.url}")
