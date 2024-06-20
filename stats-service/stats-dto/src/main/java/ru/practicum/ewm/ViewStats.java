123d78fedfde11c2147b474e741917bb1f0d86d9
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-20 21:42:07 +0300

    Fix Import

--- stats-service/stats-dto/src/main/java/ru/practicum/ewm/ViewStats.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/ViewStats.java
@@ -3,10 +3,7 @@ package ru.practicum.ewm;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
 @Builder
