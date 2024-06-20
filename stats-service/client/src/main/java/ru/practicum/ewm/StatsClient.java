75f0c250103230e4635fd436caca7baf89733686
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-20 10:55:05 +0300

    Fix

--- stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -1,22 +1,29 @@
 package ru.practicum.ewm;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.ResourceBundle;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StatsClient {
 
     private final WebClient webClient;
+    private final ResourceBundle resource = ResourceBundle.getBundle("messages");
 
     public void saveHit(EndpointHit endpointHit) {
-        webClient.post()
-                .uri("/hit")
+        String uri = resource.getString("client.hits");
+        log.info("Request on uri {} body {}", uri, endpointHit);
+        webClient
+                .post()
+                .uri(uri)
                 .bodyValue(endpointHit)
                 .retrieve()
                 .bodyToMono(EndpointHit.class)
@@ -24,14 +31,13 @@ public class StatsClient {
     }
 
     public List<ViewStats> getHit(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        String uri = resource.getString("client.stats");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String request = String.format("/stats?start=%s&end=%s&uris=%s&unique=%s",
-                start.format(formatter),
-                end.format(formatter),
-                String.join(",", uris),
-                unique
-        );
-        return webClient.get()
+        String request = String.format(uri, start.format(formatter), end.format(formatter),
+                String.join(",", uris), unique);
+        log.info("Request on uri {}", request);
+        return webClient
+                .get()
                 .uri(request)
                 .retrieve()
                 .bodyToFlux(ViewStats.class)
