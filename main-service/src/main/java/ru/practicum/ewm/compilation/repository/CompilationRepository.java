c0bd000c81b6f4a8c72c3fbc40c9b7deee6ccf90
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-21 01:02:06 +0300

    Fix table name

--- main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.ewm.compilation.model.Compilation;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
-    @Query(value = "SELECT * FROM compilation c WHERE (:pinned is null or c.pinned = :pinned)", nativeQuery = true)
+    @Query(value = "SELECT * FROM compilations c WHERE (:pinned is null or c.pinned = :pinned)", nativeQuery = true)
     Page<Compilation> findAllByPinnedIsNullOrPinned(Boolean pinned, PageRequest pageRequest);
 }
