89f4b0a340abb68c1560f754cb0a32c0b9b3cd75
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-20 11:09:30 +0300

    Fix

--- stats-service/client/src/main/java/ru/practicum/ewm/ClientConfig.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/ClientConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientConfig {
-    @Value("${stats-server.url}")
-    private String baseUrl;
+//    @Value("${stats-server.url}")
+//    private String baseUrl;
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl(baseUrl)
+                .baseUrl("http://stats-server:9090:http://localhost:9090")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
