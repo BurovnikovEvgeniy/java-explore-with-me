12f5107cca8758c71334e00e33bf4506acfc9efc
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-21 00:35:03 +0300

    Fix named parameter

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
@@ -11,28 +11,28 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<Stat, Long> {
     @Query("SELECT new ru.practicum.ewm.ViewStats(s.app, s.uri, COUNT(s.uri)) " +
             "FROM Stat s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
+            "WHERE s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(s.uri) DESC")
     List<ViewStats> findAllStats(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ewm.ViewStats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stat s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
+            "WHERE s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStats> findAllStatsUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ewm.ViewStats(s.app, s.uri, COUNT(s.uri)) " +
             "FROM Stat s " +
-            "WHERE s.uri IN ?1 AND s.timestamp BETWEEN ?2 AND ?3 " +
+            "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(s.uri) DESC")
     List<ViewStats> findAllStatsUrisIn(List<String> uris, LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ewm.ViewStats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stat s " +
-            "WHERE s.uri LIKE ?1 AND s.timestamp BETWEEN ?2 AND ?3 " +
+            "WHERE s.uri LIKE :uris AND s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStats> findAllStatsUniqueIpUrisIn(String uris, LocalDateTime start, LocalDateTime end);
