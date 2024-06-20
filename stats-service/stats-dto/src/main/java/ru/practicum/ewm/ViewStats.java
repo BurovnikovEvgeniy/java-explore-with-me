41666e9328c8985623c3110398c9595f3a2db15c
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-20 21:38:49 +0300

    Fix ViewStats

--- stats-service/stats-dto/src/main/java/ru/practicum/ewm/ViewStats.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/ViewStats.java
@@ -1,14 +1,17 @@
 package ru.practicum.ewm;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
 public class ViewStats {
     private String app;
     private String uri;
